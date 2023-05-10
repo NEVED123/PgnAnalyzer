e2f1718aca739558b4e695f644f3020eb5ea4ca5
NEVED123 <neved1234@gmail.com>
2023-05-10 15:11:45 -0400

    added games.pgn, starter program with args

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PgnAnalyzer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(args[0] + args[1]);
+    }
+}
