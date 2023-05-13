e953071f7c79f976305c1e3c404157af775f7fdf
NEVED123 <neved1234@gmail.com>
2023-05-12 21:35:45 -0400

    initialized variables in struct constructors

--- src/BlunderSpotData.cs
+++ src/BlunderSpotData.cs
@@ -7,7 +7,7 @@ struct BlunderSpotData
         count = 0;
     }
 
-    public int? moveNum {get; set;}
-    public string? san {get; set;}
+    public int moveNum {get; set;}
+    public string san {get; set;}
     public int count {get; set;}
 }
--- src/RatingData.cs
+++ src/RatingData.cs
@@ -7,10 +7,12 @@ struct RatingData
         whiteWinNum = 0;
         blackWinNum = 0;
         drawNum = 0;
+        outOfBookData = new List<OutOfBookData>();
+        blunderSpotData = new List<BlunderSpotData>();
     }
 
-    public List<OutOfBookData>? outOfBookData {get; set;}
-    public List<BlunderSpotData>? blunderSpotData {get; set;}
+    public List<OutOfBookData> outOfBookData {get; set;} = new List<OutOfBookData>();
+    public List<BlunderSpotData>? blunderSpotData {get; set;} = new List<BlunderSpotData>();
     public int? eloMin {get; set;}
     public int whiteWinNum {get; set;}
     public int blackWinNum {get; set;}
